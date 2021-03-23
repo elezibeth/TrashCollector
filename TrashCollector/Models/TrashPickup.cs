99e80084dfafd8e314aa865f2a7141ae68903082
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-23 10:50:50 -0700

    TrashPickup model complete.

--- TrashCollector/Models/TrashPickup.cs
+++ TrashCollector/Models/TrashPickup.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,14 @@ namespace TrashCollector.Models
 {
     public class TrashPickup
     {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "One time pickup date")]
+        public int Date { get; set; }
+        public int Time { get; set; }
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        
     }
 }
