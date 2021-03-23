6bda5e49efc0b970dcf8a43683627f36c43c335f
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-23 11:39:15 -0700

    Models Edited.

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -21,5 +21,17 @@ namespace TrashCollector.Models
         public string IdentityUserId { get; set; }
         [Display(Name = "Customer ID")]
         public IdentityUser IdentityUser { get; set; }
+
+        public double AmountOwed { get; set; }
+        [Display(Name = "Street")]
+        public string StreetName { get; set; }
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Display(Name = "State")]
+        public string State { get; set; }
+        [Display(Name = "Zip")]
+        public int ZipCode { get; set; }
+        [Display(Name = "Type in the day of the week as Monday, Tuesday, Wednesday, Thursday, or Friday. Service not available on weekends.")]
+        public string Day { get; set; }
     }
 }
